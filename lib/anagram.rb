13557532c11359559ca051e0a79f327381f14253
John Zonneveld <jjczon@gmail.com>
2020-04-04 21:55:26 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+
+    attr_accessor
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        list = []
+        word = @word.chars.sort{|a,b| a.casecmp(b)}.join
+        array.each do |string|
+            if string.size == word.size
+                test = string.chars.sort{|a,b| a.casecmp(b)}.join
+                if test == word
+                    list << string
+                end
+            end
+        end
+        list
+    end
+end
